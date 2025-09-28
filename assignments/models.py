59b7b1c6108e0b85fcd11002356fe9735ff7f873
iamrushier <rushikeshsurve193@gmail.com>
2025-09-28 12:23:46 +0530

    Created models - Assignment, Submission

--- assignments/models.py
+++ assignments/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.conf import settings
+from courses.models import Course, Enrollment
 
-# Create your models here.
+class Assignment(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='assignments'
+    )
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'teacher'},
+        related_name="assignments_created"
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.course.code})"
+
+
+class Submission(models.Model):
+    assignment = models.ForeignKey(
+        Assignment,
+        on_delete=models.CASCADE,
+        related_name="submissions"
+    )
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        limit_choices_to={"role": "student"},
+        related_name="submissions"
+    )
+    file = models.FileField(upload_to="submissions/")
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    grade = models.CharField(max_length=10, blank=True, null=True)
+    feedback = models.TextField(blank=True, null=True)
+    
+    class Meta:
+        unique_together = ("assignment", "student")  # one submission per student
+        ordering = ["-submitted_at"]
+
+    def __str__(self):
+        return f"{self.student.username} → {self.assignment.title}"
