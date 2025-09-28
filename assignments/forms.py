3824c53e70521dad776f256f8c86b83606c94028
iamrushier <rushikeshsurve193@gmail.com>
2025-09-28 12:41:32 +0530

    Form for assignment and submission, admin panel display

---
+++ assignments/forms.py
@@ -0,0 +1,21 @@
+# assignments/forms.py
+from django import forms
+from .models import Assignment, Submission
+
+class AssignmentForm(forms.ModelForm):
+    due_date = forms.DateTimeField(
+        required=False,
+        widget = forms.DateTimeInput(
+            attrs={
+                "type":"datetime-local",
+            }
+        )
+    )
+    class Meta:
+        model = Assignment
+        fields = ["course", "title", "description", "due_date"]
+
+class SubmissionForm(forms.ModelForm):
+    class Meta:
+        model = Submission
+        fields = ["file"]
